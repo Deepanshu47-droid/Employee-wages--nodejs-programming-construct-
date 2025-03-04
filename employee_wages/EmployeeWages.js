f6cf32f8b59a2e29e949f71c239af2bc457f0efb
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-03-04 19:17:33 +0530

    [Deepanshu malviya]- Add. Refactor the Code to write a function to get work hours

--- employee_wages/EmployeeWages.js
+++ employee_wages/EmployeeWages.js
@@ -11,6 +11,7 @@ class EmployeeAttendance {
         let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
         return (attendance === PRESENT) ? "Employee is Present" : "Employee is Absent";
     }
+
     // UC-2: Ability to Calculate Daily Employee Wage based on part-time or full-time
     calculateDailyWage() {
         const IS_PART_TIME = 1;
@@ -19,22 +20,27 @@ class EmployeeAttendance {
         const FULL_TIME_HOURS = 8;
         const WAGE_PER_HOUR = 20;
         let workType = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
-        let workingHours = 0;
 
-        switch (workType) {
-            case IS_PART_TIME:
-                workingHours = PART_TIME_HOURS;
-                break;
-            case IS_FULL_TIME:
-                workingHours = FULL_TIME_HOURS;
-                break;
-            default:
-                workingHours = 0; // Absent or No work
-        }
+        let workingHours = this.getWorkingHours(workType); 
 
         let dailyWage = workingHours * WAGE_PER_HOUR;
         return `Daily Wage is $${dailyWage}`;
     }
+
+    // UC-3: Function to get work hours based on work type  
+    getWorkingHours(workType) {  
+        const PART_TIME_HOURS = 4;  
+        const FULL_TIME_HOURS = 8;  
+
+        switch (workType) {  
+            case 1:  
+                return PART_TIME_HOURS;  
+            case 2:  
+                return FULL_TIME_HOURS;  
+            default:  
+                return 0; // No work or absent  
+        }  
+    }  
 }
 
 // Creating object of EmployeeAttendance class
@@ -46,3 +52,5 @@ console.log(employee.checkAttendance());
 // Running UC-2
 console.log(employee.calculateDailyWage());
 
+// Running UC-2 and UC-3  
+console.log(employee.calculateDailyWage());  
